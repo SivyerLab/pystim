8cc476af75032d621ba8a51eb074c40622ef41a6
Alexander <mexander@gmail.com>
2015-11-24 11:36:09 -0800

    prep to try on OSX

--- setup.py
+++ setup.py
@@ -1,23 +1,44 @@
-from distutils.core import setup
-import py2exe
+import os, sys
 import numpy, scipy
-import os
+from distutils.core import setup
 
-pref_files = []
-app_files = []
+if sys.platform=='win32':
+    import py2exe
 
-for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
-                        '\\preferences\\'):
-    f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
-    # print f1
-    pref_files.append(f1)
+    pref_files = []
+    app_files = []
 
-for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\'):
-    f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
-    # print f1
-    app_files.append(f1)
+    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
+                            '\\preferences\\'):
+        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
+        # print f1
+        pref_files.append(f1)
 
-all_files = [("psychopy\\preferences", pref_files)]
+    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\'):
+        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
+        # print f1
+        app_files.append(f1)
+
+    all_files = [("psychopy\\preferences", pref_files)]
+
+elif sys.platform=='Darwin':
+    import py2app
+
+    pref_files = []
+    app_files = []
+
+    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
+                            '\\preferences\\'):
+        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
+        # print f1
+        pref_files.append(f1)
+
+    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\'):
+        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
+        # print f1
+        app_files.append(f1)
+
+    all_files = [("psychopy\\preferences", pref_files)]
 
 setup(console=['gui.py'],
       data_files=all_files,
