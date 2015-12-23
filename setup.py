fa51d12191528a0e19fc18e5dbf8c0f246009152
Alexander Tomlinson <mexander@gmail.com>
2015-12-23 11:00:43 -0800

    Fixed setup for darwin/win32 platforms; now includes stims and ini

--- setup.py
+++ setup.py
@@ -1,54 +1,78 @@
 import os, sys
 import numpy, scipy
-from distutils.core import setup
 
 if sys.platform=='win32':
+    from distutils.core import setup
     import py2exe
 
     pref_files = []
     app_files = []
+    stim_files = []
+
+    ini_file = [".\\psychopy\\config.ini"]
+
+    for files in os.listdir(".\\psychopy\\stims\\"):
+        f1 = ".\\psychopy\\stims\\" + files
+        stim_files.append(f1)
 
     for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
                             '\\preferences\\'):
         f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
-        # print f1
         pref_files.append(f1)
 
-    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\'):
-        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
-        # print f1
-        app_files.append(f1)
+    all_files = [("psychopy\\preferences", pref_files),
+                 ("psychopy\\stims", stim_files), ('psychopy', ini_file)]
 
-    all_files = [("psychopy\\preferences", pref_files)]
+    setup(console=['gui.py'],
+          data_files=all_files,
+          windows= [
+              {
+                  'script' : 'gui.py',
+                  'icon_resources' : [(1, 'icon1.ico')]
+              }
+          ],
+          options={
+              'py2exe': {
+                  # need all these or building fails
+                  'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
+                                'scipy.linalg.*', 'scipy.integrate',
+                                'scipy.sparse.csgraph._validation',
+                                'multiprocessing', 'PIL.*',
+                                'psychopy.visual.*']
+              }
+          })
 
-elif sys.platform=='Darwin':
+elif sys.platform=='darwin':
+    from setuptools import setup
     import py2app
 
     pref_files = []
     app_files = []
+    stim_files = []
 
-    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
-                            '\\preferences\\'):
-        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
-        # print f1
+    ini_file = ["./psychopy/config.ini"]
+
+    for files in os.listdir("./psychopy/stims/"):
+        f1 = "./psychopy/stims/" + files
+        stim_files.append(f1)
+
+    for files in os.listdir('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/psychopy/preferences/'):
+        f1 = '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/psychopy/preferences/' + files
         pref_files.append(f1)
 
-    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\'):
-        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
-        # print f1
-        app_files.append(f1)
+    all_files = [("psychopy/preferences", pref_files),
+                 ("psychopy/stims", stim_files), ('psychopy', ini_file)]
 
-    all_files = [("psychopy\\preferences", pref_files)]
-
-setup(console=['gui.py'],
-      data_files=all_files,
-      options={
-          'py2exe': {
-              'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
-                            'scipy.linalg.*', 'scipy.integrate',
-                            'scipy.sparse.csgraph._validation',
-                            'multiprocessing',
-                            'PIL.*',
-                            'psychopy.visual.*']
-          }
-      })
+    setup(app=['gui.py'],
+          setup_requires=['py2app'],
+          data_files=all_files,
+          options={
+              'py2app': {
+                  'packages' : ['PIL'],
+                  'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
+                                'scipy.linalg.*', 'scipy.integrate',
+                                'scipy.sparse.csgraph._validation',
+                                'multiprocessing',
+                                'psychopy.visual.*']
+              }
+          })
