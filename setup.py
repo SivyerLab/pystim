8e8d5a8ec2073988ed9c21b755e6300c84b5b04b
Alexander Tomlinson <mexander@gmail.com>
2016-01-15 12:59:02 -0800

    gamma correction changes

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
 """
-py2exe/py2app setup script. Depending on the OS, the executables fail to
+Py2exe/py2app setup script. Depending on the OS, the executables fail to
 build without the modules added to 'includes'. This clutters the folder in
 the dist for windows, but at least it works. Icon inclusion seems buggy as well.
 
@@ -14,92 +16,95 @@ it's default dictionary.
 import os, sys, py_compile
 import numpy, scipy
 
-if sys.platform=='win32':
-    from distutils.core import setup
-    import py2exe
-    import ctypes
+def setup():
+    if sys.platform=='win32':
+        from distutils.core import setup
+        import py2exe
+        import ctypes
 
-    pref_files = []
-    app_files = []
-    stim_files = []
+        pref_files = []
+        app_files = []
+        stim_files = []
 
-    ini_file = [".\\psychopy\\config.ini"]
+        ini_file = [".\\psychopy\\config.ini"]
 
-    for files in os.listdir(".\\psychopy\\stims\\"):
-        f1 = ".\\psychopy\\stims\\" + files
-        stim_files.append(f1)
+        for files in os.listdir(".\\psychopy\\stims\\"):
+            f1 = ".\\psychopy\\stims\\" + files
+            stim_files.append(f1)
 
-    for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
-                            '\\preferences\\'):
-        f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
-        pref_files.append(f1)
+        for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy'
+                                '\\preferences\\'):
+            f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
+            pref_files.append(f1)
 
-    all_files = [("psychopy\\preferences", pref_files),
-                 ("psychopy\\stims", stim_files),
-                 ('psychopy', ini_file)]
+        all_files = [("psychopy\\preferences", pref_files),
+                     ("psychopy\\stims", stim_files),
+                     ('psychopy', ini_file)]
 
-    setup(console=['gui.py'],
-          data_files=all_files,
-          windows= [
-              {
-                  'script' : 'gui.py',
-                  'icon_resources' : [(1, 'icon1.ico')]
-              }
-          ],
-          options={
-              'py2exe': {
-                  # need all these or building fails
-                  'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
-                                'scipy.linalg.*', 'scipy.integrate',
-                                'scipy.sparse.csgraph._validation',
-                                'multiprocessing', 'PIL.*',
-                                'psychopy.visual.*']
-              }
-          })
+        setup(console=['gui.py'],
+              data_files=all_files,
+              windows= [
+                  {
+                      'script' : 'gui.py',
+                      'icon_resources' : [(1, 'icon1.ico')]
+                  }
+              ],
+              options={
+                  'py2exe': {
+                      # need all these or building fails
+                      'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
+                                    'scipy.linalg.*', 'scipy.integrate',
+                                    'scipy.sparse.csgraph._validation',
+                                    'multiprocessing', 'PIL.*',
+                                    'psychopy.visual.*']
+                  }
+              })
 
-    os.system('start robocopy "C:\Python27\Lib\site-packages\psychopy\\app" '
-              '".\dist\psychopy\\app" /E')
-    py_compile.compile('GammaCorrection.py', cfile='.\dist\GammaCorrection.pyc')
+        os.system('start robocopy "C:\Python27\Lib\site-packages\psychopy\\app" '
+                  '".\dist\psychopy\\app" /E')
 
-elif sys.platform=='darwin':
-    from setuptools import setup
-    import py2app
+    elif sys.platform=='darwin':
+        from setuptools import setup
+        import py2app
 
-    pref_files = []
-    app_files = []
-    stim_files = []
+        pref_files = []
+        app_files = []
+        stim_files = []
 
-    ini_file = ["./psychopy/config.ini"]
+        ini_file = ["./psychopy/config.ini"]
 
-    for files in os.listdir("./psychopy/stims/"):
-        f1 = "./psychopy/stims/" + files
-        stim_files.append(f1)
+        for files in os.listdir("./psychopy/stims/"):
+            f1 = "./psychopy/stims/" + files
+            stim_files.append(f1)
 
-    for files in os.listdir('/Library/Frameworks/Python.framework/Versions/2.7/'
-                            'lib/python2.7/site-packages/psychopy/preferences/'):
-        f1 = '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7' \
-             '/site-packages/psychopy/preferences/' + files
-        pref_files.append(f1)
+        for files in os.listdir('/Library/Frameworks/Python.framework/Versions/2.7/'
+                                'lib/python2.7/site-packages/psychopy/preferences/'):
+            f1 = '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7' \
+                 '/site-packages/psychopy/preferences/' + files
+            pref_files.append(f1)
 
-    all_files = [("psychopy/preferences", pref_files),
-                 ("psychopy/stims", stim_files), ('psychopy', ini_file)]
+        all_files = [("psychopy/preferences", pref_files),
+                     ("psychopy/stims", stim_files), ('psychopy', ini_file)]
 
-    setup(app=['gui.py'],
-          setup_requires=['py2app'],
-          data_files=all_files,
-          options={
-              'py2app': {
-                  'packages' : ['PIL'],
-                  'iconfile' : 'icon1.ico',
-                  'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
-                                'scipy.linalg.*', 'scipy.integrate',
-                                'scipy.sparse.csgraph._validation',
-                                'multiprocessing',
-                                'psychopy.visual.*']
-              }
-          })
+        setup(app=['gui.py'],
+              setup_requires=['py2app'],
+              data_files=all_files,
+              options={
+                  'py2app': {
+                      'packages' : ['PIL'],
+                      'iconfile' : 'icon1.ico',
+                      'includes' : ['scipy.*', 'scipy.integrate', 'scipy.special.*',
+                                    'scipy.linalg.*', 'scipy.integrate',
+                                    'scipy.sparse.csgraph._validation',
+                                    'multiprocessing',
+                                    'psychopy.visual.*']
+                  }
+              })
 
-    os.system('cp -r /Library/Frameworks/Python.framework/Versions/2.7/lib'
-              '/python2.7/site-packages/psychopy/app '
-              '.dist/gui.app/Contents/Resources/psychopy/app')
-    py_compile.compile('GammaCorrection.py', cfile='./dist/gui.app/Contents/Resources/GammaCorrection.pyc')
+        os.system('cp -r /Library/Frameworks/Python.framework/Versions/2.7/lib'
+                  '/python2.7/site-packages/psychopy/app '
+                  '.dist/gui.app/Contents/Resources/psychopy/app')
+
+
+if __name__ == '__main__':
+    setup()
