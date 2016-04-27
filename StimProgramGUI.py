016673ae7c4842b0466058cdef16d4d36d4830de
Alexander Tomlinson <mexander@gmail.com>
2016-04-27 16:31:23 -0700

    old checkerboard loading compatibility

--- StimProgramGUI.py
+++ StimProgramGUI.py
@@ -1642,6 +1642,14 @@ class ListPanel(wx.Panel):
         stim_type = self.convert_stim_type(stim.stim_type)
         params['move_type'] = stim_type
 
+        # compatibility for old checkerboards
+        if 'check_type' not in params.iterkeys():
+            if params['fill_mode'] == 'random':
+                params['fill_mode'] = 'checkerboard'
+                params['check_type'] = 'random'
+            elif params['fill_mode'] == 'checkerboard':
+                params['check_type'] = 'checkerboard'
+
         grid = self.frame.grid
 
         # post events to grid to simulate removing columns to clear text and
