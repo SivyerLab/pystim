7f0e6bbb12f10decee4868e46e15855c1b3947e3
awctomlinson <mexander@gmail.com>
2017-05-25 14:50:03 -0700

    trying to fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,14 +21,12 @@ import os
 user = os.environ.get('USERNAME')
 
 if user == 'Alex':
-    # sys.path.insert(0, os.path.abspath(r'C:\Users\Alex\PycharmProjects\SivyerLab_pyStim\pyStim'))
     sys.path.insert(0, os.path.abspath(r'C:\Users\Alex\Anaconda3\envs\stimprogram\Lib\site-packages'))
 elif user == 'bensivyer':
-    sys.path.insert(0, os.path.abspath(r'C:\Users\bensivyer\PycharmProjects\pyStim\pyStim'))
     sys.path.insert(0, os.path.abspath(r'C:\Users\bensivyer\Anaconda3\envs\sp_env\Lib\site-packages'))
-else:
-    sys.path.insert(0, os.path.abspath(r'..\..\pyStim\\'))
 
+sys.path.insert(0, os.path.abspath(r'..\..\pyStim'))
+print os.path.abspath(r'..\..\pyStim')
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
