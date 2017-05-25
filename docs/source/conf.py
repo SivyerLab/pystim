8f1f91404c0df1199411554e7093a6561b34a69d
awctomlinson <mexander@gmail.com>
2017-05-25 15:03:29 -0700

    conda in travis

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ elif user == 'bensivyer':
     sys.path.insert(0, os.path.abspath(r'C:\Users\bensivyer\Anaconda3\envs\sp_env\Lib\site-packages'))
 
 sys.path.insert(0, os.path.abspath(r'../../pyStim'))
-print os.path.abspath(r'../../pyStim')
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
